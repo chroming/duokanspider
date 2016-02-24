8960e73439dddb78299eba0d0760d127a6eefa84
chroming <guangzhishang@gmail.com>
2016-02-24 15:30:18 +0800

    多看爬虫

--- duokanspider/spiders/duokanSpider.py
+++ duokanspider/spiders/duokanSpider.py
@@ -12,14 +12,12 @@ class DuokanSpider(scrapy.Spider):
     def parse(self, response):
         #pagenumber = response.xpath('//ul[contains(@id, "bookpage")]//a[contains(@href, "list")]/@href').re(r'.*-(\d*)$')[-1]
         for bookinfo in response.xpath('//li[@class="u-bookitm1 j-bookitm"]'):
-            print bookinfo.extract()
             item = DuokanSpiderItem()
             item['bookprice'] = bookinfo.xpath('.//em/text()').extract()
             item['bookdelprice'] = bookinfo.xpath('.//del/text()').extract()
             item['bookid'] = bookinfo.xpath('.//div/a[contains(@href, "book")]/@href').re(r'(\d*)$')[0]
             item['bookname'] = bookinfo.xpath('.//img[@src]/@alt').extract()
             item['bookauthor'] = bookinfo.xpath('.//div[@class="u-author"]/span/text()').extract()
-            raw_input(item)
             yield item
 
         #get next page
